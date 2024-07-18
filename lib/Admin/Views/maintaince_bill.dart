2f5a633f6af3d6bcb61d9409a9019df2e5b9d2d6
dileep <dileeptroy45@gmail.com>
2024-07-18 18:19:16 +0530

    made changes in maintenance bill screens

--- lib/Admin/Views/maintaince_bill.dart
+++ lib/Admin/Views/maintaince_bill.dart
@@ -8,34 +8,39 @@ import '../../styles/basicstyles.dart';
 
 class MaintainceBill extends StatefulWidget {
   String? apartcode;
-  String?userid;
-  MaintainceBill({super.key, this.apartcode,this.userid});
+  String? userid;
+  MaintainceBill({super.key, this.apartcode, this.userid});
 
   @override
   State<MaintainceBill> createState() => _MaintainceBillState();
 }
 
 class _MaintainceBillState extends State<MaintainceBill> {
+  final _formKey = GlobalKey<FormState>();
   TextEditingController amount = TextEditingController();
-
   String? selectedvalue;
- @override
+
+  @override
   void initState() {
     super.initState();
- }
+  }
+
   @override
   void dispose() {
     amount.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Maintaince Bill',
-          style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w500,
-          color: Colors.white
+          'Maintenance Bill',
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
           ),
         ),
         centerTitle: true,
@@ -44,92 +49,140 @@ class _MaintainceBillState extends State<MaintainceBill> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            FutureBuilder<List<String>?>(
-              future: ApiService().getBlockName(widget.apartcode),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  List<String>? blocknames = snapshot.data;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 20),
-                      Text(
-                        "Select Block",
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.pink.shade400),
-                      ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width / 1.3,
-                        child: DropdownButtonFormField2<String>(
-                          value: selectedvalue,
-                          items: blocknames!
-                              .map((item) => DropdownMenuItem<String>(
-                            value: item,
-                            child: Text(
-                              item,
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ))
-                              .toList(),
-                          onChanged: (value) {
-                            setState(() {
-                              selectedvalue = value;
-                            });
-                          },
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
-                            hintText: 'Select Block',
-                            contentPadding: EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder<List<String>?>(
+                future: ApiService().getBlockName(widget.apartcode),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<String>? blocknames = snapshot.data;
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20),
+                        Text(
+                          "Select Block",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.pink.shade400,
                           ),
                         ),
-                      ),
-                    ],
-                  );
-                }
-                return Container();
-              },
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Enter Maintenance Bill Amount",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.pink.shade400),
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: amount,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: InputDecoration(
-                hintText: 'Enter Amount',
-                contentPadding: EdgeInsets.symmetric(vertical: 14, horizontal: 16),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
-                focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0), borderSide: BorderSide(color: Colors.black, width: 1.3)),
-                errorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0), borderSide: BorderSide(color: Colors.red, width: 1.9)),
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter amount';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  postData(widget.userid, widget.apartcode);
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 1.3,
+                          child: DropdownButtonFormField2<String>(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please select a block';
+                              }
+                              return null;
+                            },
+                            value: selectedvalue,
+                            items: blocknames!
+                                .map((item) => DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ))
+                                .toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedvalue = value;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              hintText: 'Select Block',
+                              contentPadding: const EdgeInsets.symmetric(
+                                vertical: 14,
+                                horizontal: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                  return Container();
                 },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purple.shade300,
-                  minimumSize: const Size(120, 50),
-                  textStyle: GoogleFonts.acme(fontSize: 18, fontWeight: FontWeight.w500, letterSpacing: 0.4),
-                ),
-                child: const Text('Submit',style: TextStyle(fontWeight: FontWeight.w300,color: Colors.white),),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Text(
+                "Enter Maintenance Bill Amount",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.pink.shade400,
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 1.3,
+                child: TextFormField(
+                  controller: amount,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                    hintText: 'Enter Amount',
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 16,
+                      horizontal: 16,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: const BorderSide(color: Colors.black, width: 1.3),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: const BorderSide(color: Colors.red, width: 1.9),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter amount';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      postData(widget.userid, widget.apartcode);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple.shade300,
+                    minimumSize: const Size(120, 50),
+                    textStyle: GoogleFonts.acme(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.4,
+                    ),
+                  ),
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -138,16 +191,16 @@ class _MaintainceBillState extends State<MaintainceBill> {
   void postData(String? userid, String? apartcode) async {
     String maintainceamount = amount.text.replaceAll(",", "");
 
-     var value = await ApiService().getApartmentDetails(apartcode);
-   Map<String,dynamic>data = {
-     "apartment_name":value![0].apartmentName,
-     "apartment_code": value[0].apartmentCode,
-     "block_name" : selectedvalue,
-     "admin_id" : userid,
-     "amount" : int.parse(maintainceamount),
-     "date" : DateTime.now().toString(),
+    var value = await ApiService().getApartmentDetails(apartcode);
+    Map<String, dynamic> data = {
+      "apartment_name": value![0].apartmentName,
+      "apartment_code": value[0].apartmentCode,
+      "block_name": selectedvalue,
+      "admin_id": userid,
+      "amount": int.parse(maintainceamount),
+      "date": DateTime.now().toString(),
     };
-     amount.clear();
-     ApiService().maintainceAmount(data,selectedvalue,apartcode);
-   }
+    amount.clear();
+    ApiService().maintainceAmount(data, selectedvalue, apartcode);
+  }
 }
